c950b11b3d18c0d3cc960ab029532877a348fc6f
Paulo Barros <p.barros@openvia.io>
2024-09-02 22:17:44 +0100

    update packages

---
+++ Consumer/ConsumerWithOTEL/Program.cs
@@ -0,0 +1,133 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConsumerWithOTEL;
+using ConsumerWithOTEL.Common;
+using ConsumerWithOTEL.Handlers;
+using KafkaFlow;
+using KafkaFlow.Configuration;
+using KafkaFlow.OpenTelemetry;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using OpenTelemetry;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+public class Program
+{
+
+    public async static Task Main(string[] args)
+    {
+        var services = new ServiceCollection();
+
+        const string producerName = "PrintConsole";
+        const string topicName =  "avro-topic";
+
+
+        // using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+        //     .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(
+        //         serviceName: "ConsumerApp", // Name of the consumer service
+        //         serviceVersion: "1.0.0"))
+        //     .AddSource(KafkaFlowInstrumentation.ActivitySourceName)
+        //     .AddConsoleExporter()
+        //     .AddAspNetCoreInstrumentation()
+        //     // .AddJaegerExporter(options => options.Endpoint = new Uri("http://localhost:14250")) // Jaeger's OTLP receiver
+        //     // .AddOtlpExporter()
+        //     .AddOtlpExporter(options =>
+        //     {
+        //         options.Endpoint = new Uri("http://localhost:4317"); // Jaeger's OTLP receiver
+        //         options.Protocol = OtlpExportProtocol.Grpc;
+        //     })
+        //     .Build();
+
+
+      await Host
+            .CreateDefaultBuilder(args)
+            .ConfigureLogging(logging=> logging.AddOpenTelemetry(o =>
+                {
+                    var resourceBuilder = ResourceBuilder.CreateDefault();
+                    resourceBuilder.AddService(
+                        serviceName: "ConsumerApp", // Name of the consumer service
+                        serviceVersion: "1.0.0");
+                    o.SetResourceBuilder(resourceBuilder);
+                    o.IncludeScopes = true;
+                    o.IncludeFormattedMessage = true;
+                    o.ParseStateValues = true;
+
+                    o.AddOtlpExporter(options =>
+                        {
+                            options.Endpoint =new Uri("http://localhost:4317");
+                            options.Protocol = OtlpExportProtocol.Grpc;
+                        }
+                    );
+                }))
+                .ConfigureServices(
+                (hostContext, services) =>
+                {
+                    services.AddLogging();
+                    services.AddOpenTelemetry().ConfigureResource(options => options.AddService(
+                            serviceName: "ConsumerApp", // Name of the consumer service
+                            serviceVersion: "1.0.0"
+                        ))
+                        .WithTracing(
+                            options =>
+                                options.AddSource(KafkaFlowInstrumentation.ActivitySourceName)
+                                    .AddConsoleExporter()
+                                    .AddAspNetCoreInstrumentation()
+                                    .AddOtlpExporter(
+                                        options =>
+                                        {
+                                            options.Endpoint = new Uri("http://localhost:4317"); // Jaeger's OTLP receiver
+                                            options.Protocol = OtlpExportProtocol.Grpc;
+                                            options.BatchExportProcessorOptions = new()
+                                            {
+                                                MaxQueueSize = 1000,
+                                                ScheduledDelayMilliseconds = 1000,
+                                                MaxExportBatchSize = 100
+                                            };
+                                        }
+                                            )
+                                    .AddSource(TestTraces.ActivitySourceName)
+                        );
+
+                    services.AddKafkaFlowHostedService(
+                        kafka => kafka
+                            .UseConsoleLog()
+                            .AddOpenTelemetryInstrumentation(options =>
+                            {
+                                options.EnrichConsumer = (activity, messageContext) =>
+                                {
+                                    activity.SetTag("messaging.destination.groupid",
+                                        messageContext.ConsumerContext.GroupId);
+                                };
+                            })
+                            .AddCluster(
+                                cluster => cluster
+                                    .WithBrokers(new[]
+                                    {
+                                        "localhost:9092"
+                                    })
+                                    .WithSchemaRegistry(config => config.Url = "localhost:8081")
+                                    .AddConsumer(
+                                        consumer => consumer
+                                            .Topic(topicName)
+                                            .WithGroupId("avro-group-id")
+                                            .WithBufferSize(100)
+                                            .WithWorkersCount(4)
+                                            .AddMiddlewares(
+                                                middlewares => middlewares
+                                                    .AddSchemaRegistryAvroDeserializer()
+                                                    .AddTypedHandlers(h => h.AddHandler<PrintConsoleHandler>())
+                                            )
+                                    )
+                            )
+                    );
+                })
+
+            .Build()
+            .RunAsync();
+
+    }
+}
---
+++ Consumer/ConsumerWithRetries/Program.cs
@@ -0,0 +1,124 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConsumerWithRetries.ContractResolver;
+using ConsumerWithRetries.Exceptions;
+using ConsumerWithRetries.Handlers;
+using KafkaFlow;
+using KafkaFlow.Retry;
+using KafkaFlow.Retry.MongoDb;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+using SchemaRegistry;
+
+
+public class Program
+{
+    const string avroTopic = "avro-topic";
+    const string mongoDbConnectionString = "mongodb://root:root@localhost:27017/";
+    const string mongoDbDatabaseName = "kafka_flow_retry_durable_sample";
+    const string mongoDbRetryQueueCollectionName = "RetryQueues";
+    const string mongoDbRetryQueueItemCollectionName = "RetryQueueItems";
+    const string mongodbRetryTopic = "sample-kafka-flow-retry-durable-mongodb-avro-topic";
+
+    public static void Main(string[] args)
+    {
+        Host
+            .CreateDefaultBuilder(args)
+            .ConfigureServices(
+                (hostContext, services) =>
+                {
+                    services
+                        .AddKafkaFlowHostedService(
+                            kafka => kafka
+                                .UseConsoleLog()
+                                .AddCluster(
+                                    cluster => cluster
+                                        .WithBrokers(new[] { "localhost:9092" })
+                                        .WithSchemaRegistry(config => config.Url = "localhost:8081")
+                                        .CreateTopicIfNotExists(avroTopic, 1, 1)
+                                        .CreateTopicIfNotExists(mongodbRetryTopic, 1,1)
+                                        .AddConsumer(
+                                                consumer => consumer
+                                                    .Topic(avroTopic)
+                                                    .WithGroupId("sample-consumer-kafka-flow-retry-durable-mongodb-avro")
+                                                    .WithName("kafka-flow-retry-durable-mongodb-avro-consumer")
+                                                    .WithBufferSize(10)
+                                                    .WithWorkersCount(1)
+                                                    .WithAutoOffsetReset(AutoOffsetReset.Latest)
+                                                                            .AddMiddlewares(
+                                                                                middlewares => middlewares
+                                                                                    .AddSchemaRegistryAvroDeserializer()
+                                                                                    .RetryDurable(
+                                                                                        configure => configure
+                                                                                            .Handle<RetryDurableTestException>()
+                                                                                            .WithMessageType(typeof(AvroLogMessage))
+                                                                                            .WithMessageSerializeSettings(new JsonSerializerSettings()
+                                                                                            {
+                                                                                                ContractResolver = new WritablePropertiesOnlyResolver()
+                                                                                            })
+                                                                                            .WithMongoDbDataProvider(
+                                                                                                mongoDbConnectionString,
+                                                                                                mongoDbDatabaseName,
+                                                                                                mongoDbRetryQueueCollectionName,
+                                                                                                mongoDbRetryQueueItemCollectionName)
+                                                                                            //make it simple retry before make it durable
+                                                                                            .WithRetryPlanBeforeRetryDurable(
+                                                                                                configure => configure
+                                                                                                    .TryTimes(3)
+                                                                                                    .WithTimeBetweenTriesPlan(
+                                                                                                        TimeSpan.FromMilliseconds(250),
+                                                                                                        TimeSpan.FromMilliseconds(500),
+                                                                                                        TimeSpan.FromMilliseconds(1000))
+                                                                                                    .ShouldPauseConsumer(false)
+                                                                                            )
+                                                                                            .WithEmbeddedRetryCluster(
+                                                                                                cluster,
+                                                                                                configure => configure
+                                                                                                    .WithRetryTopicName(
+                                                                                                        mongodbRetryTopic)
+                                                                                                    .WithRetryConsumerBufferSize(4)
+                                                                                                    .WithRetryConsumerWorkersCount(1)
+                                                                                                    .WithRetryConsumerStrategy(
+                                                                                                        RetryConsumerStrategy.GuaranteeOrderedConsumption)
+                                                                                                    .WithRetryTypedHandlers(
+                                                                                                        handlers => handlers
+                                                                                                            .WithHandlerLifetime(InstanceLifetime.Transient)
+                                                                                                            .AddHandler<AvroMessageHandler>()
+                                                                                                    )
+                                                                                                    .Enabled(true)
+                                                                                            )
+                                                                                            .WithPollingJobsConfiguration(
+                                                                                                configure => configure
+                                                                                                    .WithSchedulerId("retry-durable-mongodb-polling-id")
+                                                                                                    //Retry polling job
+                                                                                                    .WithRetryDurablePollingConfiguration(
+                                                                                                        configure => configure
+                                                                                                            .WithCronExpression("0 0/1 * 1/1 * ? *") //Every minute
+                                                                                                            .WithExpirationIntervalFactor(1)
+                                                                                                            .WithFetchSize(10)
+                                                                                                            .Enabled(true)
+                                                                                                    )
+                                                                                                    //Cleanup polling job
+                                                                                                    .WithCleanupPollingConfiguration(
+                                                                                                        configure => configure
+                                                                                                            .WithCronExpression("0 0 * 1/1 * ? *") //Every hour
+                                                                                                            .WithRowsPerRequest(1048)
+                                                                                                            .WithTimeToLiveInDays(60)
+                                                                                                            .Enabled(true)
+                                                                                                    )
+                                                                                            ))
+                                                                                    .AddTypedHandlers(
+                                                                                        handlers => handlers
+                                                                                            .WithHandlerLifetime(InstanceLifetime.Transient)
+                                                                                            //.AddHandler<AvroMessageHandler>())
+                                                                                            .AddHandler<AvroMessageThrowsExceptionHandler>())
+                                        )
+
+                                )
+
+                        ));
+                })
+            .Build()
+            .Run();
+    }
+}
